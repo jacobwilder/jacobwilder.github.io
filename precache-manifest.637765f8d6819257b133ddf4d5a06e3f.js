b2817ab856d5fa05f5930cefcb5f990d8604a23a
Wilder8 <jacob.wilder826@gmail.com>
2019-12-17 20:41:02 -0500

    Updates

---
+++ precache-manifest.637765f8d6819257b133ddf4d5a06e3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9bc3983b23474a6da560",
+    "url": "/static/js/main.9bc3983b.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "9bc3983b23474a6da560",
+    "url": "/static/css/main.7c0b8635.chunk.css"
+  },
+  {
+    "revision": "30a225321148ad4bb6b986dee363631e",
+    "url": "/index.html"
+  }
+];
